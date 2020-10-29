88ca47ddcbf8e1bbb25fd94288de76dbeb826095
habibpadilla <habibpadilla@bitbucket.org>
2020-10-29 03:04:39 -0500

    commit

--- App.vue
+++ App.vue
@@ -2,56 +2,167 @@
     <div>
         <div class="row mr-0">
             <div class="col-xl-2 col-sm-12 col-md-0 pr-0 border-right shadow pl-0">       
-                    <div  class="row pt-2 m-0" style="width:100%">
-                        <button v-on-click="getTodos">Registro de Comprador</button>
-                        <button v-on-click="link('disponibilidad')">Boletas disponible</button>
+                    <div  class="row pt-2 m-0" style="width:100%;text-align: center">
+                        <button @click="link('registro')">Registro de Comprador</button>
+                        <button @click="link('disponibilidad')">Boletas disponible</button>
+                        <button @click="link('reservar')">Reservar boleta</button>
                     </div>
                 
             </div>
-            <div class="col-xl-10 col-sm-12 col-md-10 pt-3">
+            <div class="col-xl-10 col-sm-12 col-md-10 pt-3" style="text-align: center">
                 <div class="row mt-3 mb-4" v-show="accion === 'disponibilidad'">
-                    <table>
+                    <table  align="center">
                     <tbody>
-                        <tr v-for="todo in todos" :key="todo.id">
-                            <td>{{todo.id}}</td>
-                            <td>{{todo.names}}</td>
+                        <tr>Boletas Disponible</tr>
+                        <tr v-for="allTicket in allTickets" :key="allTicket.id">
+                            <td>{{allTicket.names}}</td>
                         </tr>
                     </tbody>
                     </table>
                 </div>
-                <div class="row mt-3 mb-4" v-show="accion === 'reservar'">
+                <div class="row mt-3 mb-4" v-show="accion === 'registro'">
                     <div class="col-xl-10 col-sm-12 col-md-10 p-2 pb-2 offset-1 border rounded">       
-                        <form @submit.prevent="update">
-                            <div class="row mt-3 offset-1 text-left">
-                                <div class="col-md-12 mb-4 text-left text-primary"><h4><span class="nav-bar-icon mr-2 mdi mdi-36px mdi-book-open-outline"></span>{{$t('Reservar Salones')}}</h4></div>
+                        <form>
+                            <div class="form-group row mb-3 ml-0 mr-0">
+                                <br>
+                                <table align="center">
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left" for="namesfor">
+                                            {{'REGISTRO DE COMPRADOR'}} :
+                                        </label>
+                                    </td>
+                                </tr>
+                                </table>
+                                <br>                                  
                             </div>
-                            <div class="form-group row mb-4 ml-0 mr-0">
-                                <label class="col-md-2 col-form-label text-left pt-3 " for="residente">
-                                    <span class="nav-bar-icon mr-2 mdi mdi-calendar"></span>{{$t('Residente')}}:</label>
-                                <div class="col-md-10 p-0">
-                                    <select v-model="Reservafm.residente" class="form-control">
-                                        <option :value="residen.id" :key="residen.id" v-for="residen in resid">{{ residen.name }}</option>                                
-                                    </select>
-                                </div> 
+                                
+                            <div class="form-group row mb-3 ml-0 mr-0">
+                                <br>
+                                <table align="center">
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left" for="namesfor">
+                                            {{'Nombres'}} :
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <div class="col-md-10 p-0">
+                                            <input type="text" class="form-control" id="names" placeholder="Ingresar Nombres" v-model="buyer.names" required>
+                                        </div>
+                                    </td>
+                                </tr>
+                                </table>                                 
                             </div>
-                            
-                            <div class="form-group row mb-4 ml-0 mr-0 mt-4">  
-                               <div class="col-md-4 p-0"></div>
-                                <div class="col-md-5 p-0 mt-2">
-                                    <button class="btn btn-success"  @click="insert">
-                                        <span >Guardar reserva del salon</span>
+                            <div class="form-group row mb-3 ml-0 mr-0">
+                                <table align="center">
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left" for="surnamesfor">
+                                            {{'Apellidos'}} :</label>
+                                    </td>
+                                    <td>
+                                        <div class="col-md-10 p-0">
+                                            <input type="text" class="form-control" id="names" placeholder="Ingresar Apellidos" v-model="buyer.surnames" required>
+                                        </div>  
+                                    </td>
+                                </tr>
+                                </table> 
+                                    
+                            </div>
+                            <div class="form-group row mb-3 ml-0 mr-0">
+                                <table align="center">
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left " for="descripcion">
+                                            {{'Identificacion'}}:</label>
+                                    </td>
+                                    <td>
+                                        <div class="col-md-10 p-0">
+                                            <input type="number" class="form-control" id="identification" placeholder="Ingresar Identificacion" v-model="buyer.identification" required>
+                                        </div> 
+                                    </td>
+                                </tr>
+                                </table> 
+                            </div>
+                            <div class="form-group row mb-3 ml-0 mr-0">
+                                <table align="center">
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left" for="emailfor">
+                                            {{'Correo'}} :</label>
+                                    </td>
+                                    <td>
+                                        <div class="col-md-10 p-0">
+                                            <input type="text" class="form-control" id="email" placeholder="Ingresar Correo" v-model="buyer.email" required>
+                                        </div>
+                                    </td>
+                                </tr>
+                                </table>   
+                            </div>
+                            <div class="col-md-5 p-0 mt-2">
+                                <br>
+                                    <button class="btn btn-success"  @click="insertBuyers">
+                                        <span >Guardar comprador</span>
                                     </button>
-                                </div>
-                                <div class="col-md-3 p-0"></div>
                             </div>
                             
                         </form>
                     </div>
-                    <div class="col-xl-11 col-sm-11 col-md-11 pr-0 ml-5">       
-                        <div class="pl-3 pt-3 ml-1">                
-                            <pagination :isPagination="isPagination" :actions="actions" :searchable="searchable" :uri="uri6" :columns="columns6">                          
-                            </pagination>
-                        </div>
+                </div>
+                <div class="row mt-3 mb-4" v-show="accion === 'reservar'">
+                    <div class="col-xl-10 col-sm-12 col-md-10 p-2 pb-2 offset-1 border rounded">       
+                        <form>
+                            <div class="form-group row mb-3 ml-0 mr-0">
+                                <br>
+                                <table align="center">
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left" for="namesfor">
+                                            {{'RESERVA DE BOLETAS'}} :
+                                        </label>
+                                    </td>
+                                </tr>
+                                </table>
+                                <br>                                  
+                            </div>
+                            <table align="center">
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left pt-3 " for="buysfor">
+                                                {{'Comprador'}}:</label>
+                                    </td>
+                                    <td>
+                                        <div class="col-md-10 p-0">
+                                                <select v-model="Reservafm.buyer" class="form-control">
+                                                    <option :value="allBuyer.id" :key="allBuyer.id" v-for="allBuyer in allBuyers">{{ allBuyer.names }}</option>                                
+                                                </select>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label class="col-md-2 col-form-label text-left pt-3 " for="ticketfor">
+                                                {{'Boletas Disponibles'}}:</label>
+                                    </td>
+                                    <td>
+                                        <div class="col-md-10 p-0">
+                                                <select v-model="Reservafm.ticket" class="form-control">
+                                                    <option :value="allTicket.id" :key="allTicket.id" v-for="allTicket in allTickets">{{ allTicket.names }}</option>                                
+                                                </select>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+
+
+                            <div class="col-md-5 p-0 mt-2">
+                                <br>
+                                    <button class="btn btn-success"  @click="insertReservation">
+                                        <span >Guardar reserva</span>
+                                    </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -67,26 +178,76 @@ import axios from 'axios'
 export default {
     data(){
         return {
-            todos:null
+            todos:null,
+            accion:"disponibilidad",
+            Reservafm:{
+                buyer:null,
+                ticket:null
+            },
+            buyer:{
+                names:null,
+                surnames:null,
+                identification:null,
+                email:null
+            }
         }
     },
     mounted(){
-            console.log('Hola mundo desde mounted')
-            this.getTodos();
+            this.getAllTicketsAvailable();
+            this.getAllBuyers();
+    },
+    created() {
+        this.getAllTicketsAvailable()
     },
     methods: {
-        getTodos(){
+        getAllTicketsAvailable(){
             console.log('aca va el codigo get todos')
             axios
             .get('http://127.0.0.1:8000/api/tickets')
             .then(response=>{
-                this.todos=response.data
+                this.allTickets=response.data
                 console.log(response)
             })
             .catch( e=> console.log(e))
         },
-        link(accion = "general"){
-            console.log("Prueba")
+        getAllBuyers(){
+            axios
+            .get('http://127.0.0.1:8000/api/buyers')
+            .then(response=>{
+                this.allBuyers=response.data
+                console.log(response)
+            })
+            .catch( e=> console.log(e))
+        },
+        insertBuyers(){
+            let insertTableB = {
+                names:this.buyer.names,
+                surnames:this.buyer.surnames,
+                identification:this.buyer.identification,
+                email:this.buyer.email
+            }
+            console.log(insertTableB)
+            axios
+            .post('http://127.0.0.1:8000/api/buyers',insertTableB)
+            .then(response=>{
+                console.log("pruebas1"+response)
+            })
+            .catch( e=> console.log("pruebas2"+e))
+        },
+        insertReservation(){
+            let insertTableR = {
+                id_buyer:this.Reservafm.buyer,
+                id_ticket:this.Reservafm.ticket
+            }
+            axios
+            .post('http://127.0.0.1:8000/api/ticket_assignments',insertTableR)
+            .then(response=>{
+                console.log(response)
+            })
+            .catch( e=> console.log(e))
+        },
+        link(accion = "reserva"){
+            console.log("Pruebass "+accion)
             this.accion = accion
         },
     }
